c6bec694cd21e67a29fbcf02f81d4f19d0925e54
hiromushinjo <hiromu.4602@gmail.com>
2025-11-14 21:29:56 +0900

    Separate production and development environments

--- src/php/DatabaseConfig.php
+++ src/php/DatabaseConfig.php
@@ -11,18 +11,24 @@
          * @return \PDO PDOインスタンス
          */
         public function connection(): \PDO{
+        
+            //本番環境と開発環境の分岐
+            $host = $_SERVER['HTTP_HOST'] ?? '';
+            if(strpos($host, 'localhost') !== false) {
+                
+                $dsn = "pgsql:host={$_ENV['PGHOST']};port=21962;dbname={$_ENV['PGDATABASE']}";
+                $user = $_ENV['PGUSER'];
+                $password = $_ENV['PGPASSWORD'];
+            }else{
 
-            // $dsn = "pgsql:host={$_ENV['PGHOST']};port=21962;dbname={$_ENV['PGDATABASE']}";
-            // $user = $_ENV['PGUSER'];
-            // $password = $_ENV['PGPASSWORD'];
+                $host = getenv('PGHOST');
+                $database = getenv('PGDATABASE');
+                
+                $dsn = "pgsql:host={$host};port=21962;dbname={$database}";
+                $user = getenv('PGUSER');
+                $password = getenv('PGPASSWORD');
+            }
 
-            $host = getenv('PGHOST');
-            $database = getenv('PGDATABASE');
-            
-            $dsn = "pgsql:host={$host};port=21962;dbname={$database}";
-            $user = getenv('PGUSER');
-            $password = getenv('PGPASSWORD');
-            
             try{
                 $pdo = new \PDO($dsn, $user, $password);
                 $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
@@ -33,10 +39,6 @@
                 echo $e->getMessage();
                 var_dump($host, $database, $dsn, $user, $password);
                 
-            }catch(Exception $e){
-                //エラーページ表示（後日実装）
-                echo $e->getMessage();
-                var_dump($host, $database, $dsn, $user, $password);
             }
             
             return $pdo;
