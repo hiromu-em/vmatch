6757cb1301adfea4add220f8a83db5752e9c4846
hiromushinjo <hiromu.4602@gmail.com>
2025-11-14 20:35:46 +0900

    Add conditional branching based on HTTP_HOST

--- src/php/DatabaseConfig.php
+++ src/php/DatabaseConfig.php
@@ -10,10 +10,23 @@
          * @return \PDO PDOインスタンス
          */
         public function connection(): \PDO{
-            
+
+        $httpHost = $_SERVER['HTTP_HOST'] ?? '';
+        if(strpos($httpHost, 'localhost') !== false) {
             $dsn = "pgsql:host={$_ENV['PGHOST']};port=21962;dbname={$_ENV['PGDATABASE']}";
             $user = $_ENV['PGUSER'];
             $password = $_ENV['PGPASSWORD'];
+        
+        }else{
+            
+            $host = getenv("PGHOST");
+            $database = getenv("PGDATABASE");
+            
+            $dsn = "pgsql:host={$host};port=21962;dbname={$database}";
+            $user = getenv('PGUSER');
+            $password = getenv('PGPASSWORD');
+        }
+
             
             try{
                 $pdo = new \PDO($dsn, $user, $password);
