e111d67c0a812765335b084791ba180be05dc819
hiromushinjo <hiromu.4602@gmail.com>
2025-11-22 21:36:01 +0900

    Fix title

--- src/php/NewUserRegistration/register.php
+++ src/php/NewUserRegistration/register.php
@@ -48,7 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vmatch</title>
+    <title>Vmatch-新規登録-</title>
 </head>
 
 <body>
