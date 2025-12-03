284e5ee1641f39a7bcc89e24fc5bf5a2dedbcac0
hiromushinjo <hiromu.4602@gmail.com>
2025-12-03 18:23:00 +0900

    [feat]:twitter認証完了時にユーザー情報を取得・登録する機能を追加

--- src/php/Oauth/twitterOauth.php
+++ src/php/Oauth/twitterOauth.php
@@ -2,12 +2,16 @@
 declare(strict_types=1);
 
 use Vmatch\Oauth\TwitterAuthorization;
+use Vmatch\UserAuthentication\UserAuthentication;
 
 require_once __DIR__ . '/../../../vendor/autoload.php';
 
 session_start([
     'use_strict_mode' => 1
 ]);
+
+const DASHBOARD = '../dashboard.php';
+const PROFILESETTNG = '../UserAuthentication/profileSetting.php';
 
 $TwitterAuthorization = new TwitterAuthorization();
 
@@ -20,12 +24,28 @@ if (isset($_SESSION['access_token'])) {
         $access_token['oauth_token_secret']
     );
 
-    $user = $connection->get("account/verify_credentials", [
+    $user = get_object_vars($connection->get("account/verify_credentials", [
         'include_email' => 'true',
         'skip_status' => 'true',
         'include_entities' => 'false'
-    ]);
+    ]));
 
+    $userAuthentication = new UserAuthentication();
+    if ($userAuthentication->providerIdExists($user['id_str'])) {
+
+        // IDが存在する場合、ダッシュボードへリダイレクト
+        header('Location:' . filter_var(DASHBOARD, FILTER_SANITIZE_URL));
+        exit;
+    }
+
+    $userAuthentication->registerEmail($user['email']);
+
+    $userId = $userAuthentication->userInfoSearch($user['email']);
+
+    $userAuthentication->linkProviderUserId($userId, $user['id_str'], 'twitter');
+
+    // IDが存在しない場合、プロフィール設定へリダイレクト
+    header('Location:' . filter_var(PROFILESETTNG, FILTER_SANITIZE_URL));
     exit;
 }
 
