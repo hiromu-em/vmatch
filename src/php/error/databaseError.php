22d9e9f36ebee5b8c2d0ce15995c5dbf6541e3d8
hiromushinjo <hiromu.4602@gmail.com>
2025-12-04 18:22:56 +0900

    [feat]:DB接続エラー時に表示するエラーページを追加

---
+++ src/php/error/databaseError.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>サービスエラー Vmatch</title>
+</head>
+
+<body>
+    <p>現在、システムに問題が発生しています。<br>時間をおいて再度お試しください。</p>
+</body>
+
+</html>
