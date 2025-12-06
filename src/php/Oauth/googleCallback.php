f619f5ca4acc789a7bf8be014fc6d39cfdd51ff5
hiromushinjo <hiromu.4602@gmail.com>
2025-12-07 08:49:37 +0900

    コードを整形

--- src/php/Oauth/googleCallback.php
+++ src/php/Oauth/googleCallback.php
@@ -7,20 +7,17 @@ require_once __DIR__ . '/../../../vendor/autoload.php';
 
 session_start(['use_strict_mode' => 1]);
 
-$googleAuthorization = new GoogleAuthorization();
-$client = $googleAuthorization->clientConfig();
-
 // CSRF対策：stateを検証
 if (isset($_GET['state']) && $_GET['state'] !== $_SESSION['google_oauth_state']) {
-
+    
     unset($_SESSION['google_oauth_state'], $_SESSION['google_code_verifier']);
     $clearUrl = strtok($_SERVER['REQUEST_URI'], '?');
-
+    
     header('Location: ' . filter_var($clearUrl, FILTER_SANITIZE_URL));
     exit;
 
 } elseif (!isset($_GET['state']) || !isset($_GET['code'])) {
-
+    
     // 不正なアクセスの処理
     http_response_code(401);
     include_once __DIR__ . '/../error/oauthError.php';
@@ -32,6 +29,9 @@ if (isset($_GET['error'])) {
     header('Location: ' . filter_var('/', FILTER_SANITIZE_URL));
     exit;
 }
+
+$googleAuthorization = new GoogleAuthorization();
+$client = $googleAuthorization->clientConfig();
 
 //コードをアクセストークンと交換
 if (isset($_GET['code'])) {
