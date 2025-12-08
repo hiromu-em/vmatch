4723d81b5df0b5897a49837945e92046bd309136
hiromushinjo <hiromu.4602@gmail.com>
2025-12-08 10:25:14 +0900

    [fix]:ユーザーが認可を拒否、トークン照合における条件分岐ロジックを想定通りに修正

--- src/php/Oauth/twitterCallback.php
+++ src/php/Oauth/twitterCallback.php
@@ -9,28 +9,31 @@ session_start([
     'use_strict_mode' => 1
 ]);
 
+// ユーザーが認可を拒否した処理
+if (isset($_GET['denied'])) {
+
+    header('Location: ' . filter_var('/', FILTER_SANITIZE_URL));
+    exit;
+}
+
 // トークンの照合
-if (isset($_GET['oauth_token']) && $_SESSION['oauth_token'] !== $_GET['oauth_token']) {
+if ($_SESSION['oauth_token'] !== $_GET['oauth_token']) {
 
     unset($_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
     $clearUrl = strtok($_SERVER['REQUEST_URI'], '?');
 
+    // セッション情報やパラメータをクリアにして元のURLへリダイレクト
     header('Location: ' . filter_var($clearUrl, FILTER_SANITIZE_URL));
     exit;
 
-} elseif (!isset($_GET['oauth_token']) || !isset($_GET['oauth_verifier'])) {
+} elseif (!isset($_GET['oauth_token'])) {
 
-    // 不正なアクセスの処理
+    // 不正なアクセス処理    
     http_response_code(401);
     include_once __DIR__ . '/../error/oauthError.php';
     exit;
 }
 
-// アクセス拒否の処理
-if (isset($_GET['denied'])) {
-    header('Location: ' . filter_var('/', FILTER_SANITIZE_URL));
-    exit;
-}
 
 $twitterAuthorization = new TwitterAuthorization();
 $twitterAuthorization->createTwitterConnection($_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
