392ae92efb310b25758fe3bcd6432619886f2a9c
hiromushinjo <v.versehub@gmail.com>
2025-12-29 21:45:13 +0900

    add: GoogleAuthorizationの単体テストを行うため、GoogleAuthorizationTestを追加

---
+++ tests/GoogleAuthorizationTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+use Vmatch\Oauth\GoogleAuthorization;
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+class GoogleAuthorizationTest extends TestCase
+{
+
+}
