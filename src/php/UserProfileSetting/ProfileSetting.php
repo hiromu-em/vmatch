25825e7ccbcd14c0177649ff6bb464bac796f913
hiromushinjo <v.versehub@gmail.com>
2026-01-03 13:28:41 +0900

    feat: プロフィール設定インターフェースと実装を追加

---
+++ src/php/UserProfileSetting/ProfileSetting.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace Vmatch\UserProfileSetting;
+
+// use
+use Vmatch\UserProfileSetting\ProfileSettingInterface;
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+class ProfileSetting implements ProfileSettingInterface
+{
+    private array $userProfile = [];
+
+    /**
+     * ユーザー名を設定する
+     */
+    public function setName(string $name): void
+    {
+        $this->userProfile['name'] = $name;
+    }
+
+    /**
+     * 活動場所を設定する
+     */
+    public function setActivityPlaces(bool $youtube, bool $twitch): void
+    {
+        $this->userProfile['activityYoutube'] = $youtube;
+        $this->userProfile['activityTwitch'] = $twitch;
+    }
+
+    /**
+     * ソーシャルメディアのURLを設定する
+     */
+    public function setSocialMediaUrls(string ...$snsUrls): void
+    {
+        $this->userProfile['twitterUrl'] = $snsUrls[0];
+        $this->userProfile['youtubeUrl'] = $snsUrls[1];
+        $this->userProfile['twitchUrl'] = $snsUrls[2];
+    }
+}
---
+++ src/php/UserProfileSetting/ProfileSettingInterface.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Vmatch\UserProfileSetting;
+
+interface ProfileSettingInterface
+{
+    public function setName(string $name): void;
+
+    public function setActivityPlaces(bool $youtube, bool $twitch): void;
+
+    public function setSocialMediaUrls(string ...$snsUrls): void;
+
+
+}
