a01ba4cb397f6e91d533f85a0c8c7d5dd1e6920e
hiromushinjo <v.versehub@gmail.com>
2026-01-13 21:12:53 +0900

    change: 全てのリクエストをindex.phpに集約するように変更

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../routers/routers.php';
+
+$router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
