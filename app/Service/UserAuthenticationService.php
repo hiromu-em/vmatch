ed92a74db513240285e6c3f75658f53712bc2e1b
hiromushinjo <v.versehub@gmail.com>
2026-01-18 20:13:13 +0900

    rename: ユーザー認証に関わる機能を持つクラス名に変更

---
+++ app/Service/UserAuthenticationService.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace Service;
+
+use Model\UserAuthentication;
+use Vmatch\FormValidation;
+
+/**
+ * ユーザー認証に関わるクラス
+ */
+class UserAuthenticationService
+{
+
+    public function __construct(private UserAuthentication $userAuthentication, private FormValidation $formValidation)
+    {
+    }
+
+    public function register(string $email)
+    {
+    }
+
+    /**
+     * メールアドレスの検証を実行する。</br>
+     * メール形式の検証とメールアドレスがDBに存在するか検証する
+     */
+    public function executeEmailValidation(string $email, string $authType): bool
+    {
+        $this->formValidation->validateEmail($email);
+
+        if ($this->formValidation->hasErrorMessages()) {
+            $this->getErrorMessage($this->formValidation->getErrorMessage());
+            return false;
+        }
+
+        $isExistsByEmail = $this->userAuthentication->existsByEmail($email, $authType);
+
+        if ($isExistsByEmail) {
+            $this->getErrorMessage($this->userAuthentication->getErrorMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getErrorMessage(string $message): string
+    {
+        return $message;
+    }
+}
