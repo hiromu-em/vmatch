d3f64763f8d4cb88769a91b96d9867c2597c61e9
hiromushinjo <v.versehub@gmail.com>
2026-01-18 10:42:01 +0900

    add: ユーザー登録に関わるserviceクラスを追加

---
+++ app/Service/UserRegister.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Service;
+
+class UserRegister
+{
+
+    public function register(string $email)
+    {
+
+    }
+
+    public function isEmailRegistered()
+    {
+
+    }
+}
