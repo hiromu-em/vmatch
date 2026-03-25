54441c91e4ca1d86fee473a3ac985019b8512de7
hiromushinjo <v.versehub@gmail.com>
2026-03-26 08:22:47 +0900

    取得したdataを画面上に描画する

--- app/Controller/DashboardController.php
+++ app/Controller/DashboardController.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 namespace Controller;
 
 use Core\ViewRenderer;
+use Service\DashboardService;
 
 class DashboardController
 {
-    public function showDashboard(ViewRenderer $viewRenderer){
-        $viewRenderer->render('dashboard');
+    public function showDashboard(ViewRenderer $viewRenderer, DashboardService $dashboardService)
+    {
+        $vtuberChannelList['vtuberChannels'] = $dashboardService->getAllVtuberData();
+
+        $viewRenderer->render('dashboard', $vtuberChannelList);
     }
 }
--- public/resources/views/dashboard.php
+++ public/resources/views/dashboard.php
@@ -8,6 +8,37 @@
 </head>
 
 <body>
+    <header id="header">
+        <h1><a href="/dashboard">Vminder</a></h1>
+        <form action="/logout" method="get">
+            <button type="submit" class="logout-button">ログアウト</button>
+        </form>
+    </header>
+    <main class="vtuber-reminder-container">
+        <div class="group-section">
+            <div class="hololive-group-tab">
+                <h3>ホロライブ</h3>
+            </div>
+        </div>
+        <div class="search-section">
+            <input type="search" id="member-search-box" placeholder="メンバー検索" autocomplete="off">
+        </div>
+        <div class="reminder-select-list">
+
+            <?php foreach ($vtuberChannels as $vtuberChannel): ?>
+                <form action="/reminder-register" method="post" id="reminder-select-checkbox">
+                    <div class="reminder-card-section">
+                        <img src="<?= htmlspecialchars($vtuberChannel['thumbnail_url']) ?>"
+                            alt="<?= htmlspecialchars($vtuberChannel['name']) ?>">
+                        <p class="vtuber-name-content"><?= htmlspecialchars($vtuberChannel['name']) ?></p>
+                        <input type="checkbox" name="selected_members[]"
+                            value="<?= htmlspecialchars($vtuberChannel['channel_id']) ?>">
+                    </div>
+                </form>
+            <?php endforeach ?>
+
+        </div>
+    </main>
 
 </body>
 
